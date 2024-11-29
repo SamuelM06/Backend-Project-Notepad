40acdf7f205427877cb890f596e935d1348454ec
Samuel Mena <samuelmena2406@gmail.com>
2024-11-29 14:36:21 -0500

    Conexion DB y Agrega y Elimina usuarios en DB

--- main.py
+++ main.py
@@ -1,46 +1,64 @@
+from typing import Union
 from fastapi import FastAPI
 from models import User, Notes
+from database import create_db_and_tables, SessionDep
+from fastapi import HTTPException
+
+
+create_db_and_tables()
+
 app = FastAPI()
 
 app.title = "Backend-Project-Notepad"
 
-#Home 
+# Root
 @app.get("/", tags=["Root"])
 def root():
     return {"Mensaje:": "Bienvenido a Backend-Project-Notepad"}
 
-#User
-#register_user
-@app.get("/user",tags=["User"])
-def create_user(user: User):
-    return {""}
+# User
+@app.post("/user", tags=["User"])
+def create_user(user: User, session: SessionDep):
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return {"message" : "Usuario creado exitosamente","data": user ,}
+    
 
-#delete_user
-@app.delete("/user/{user_id}",tags=["User"])
-def delete_user(user_id: int, user: User):
-    return {""}
+@app.delete("/user/{user_id}", tags=["User"])
+def delete_user(user_id:int, session: SessionDep): 
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    session.delete(user)
+    session.commit()
+    return {"Usuario eliminado": True}
 
 
+#Probando metodos primero en usuario
 
-#Notes
 
-app.get("/notes", tags=["Notes"])
-def search_notes(note:Notes):
-    return{""}
-
-app.get("/notes/{IdN}", tags=["Notes"])
-def search_notes_id(note_id):
-    return{"mesaage": f"Nota {note_id}"}
-
+# Notes: Crear nota
 @app.post("/notes", tags=["Notes"])
-def create_note(note: Notes):
-    return {"message": "Nota creada", "data": note}
+def create_note(note: Notes):  
+    return {"message": "Nota creada exitosamente"}
 
-@app.put("/notes/{note_id}",tags=["Notes"])
-def update_note(note_id: int, note: Notes):
-    return {"message": f"Nota {note_id} actualizada", "data": note}
+# Notes: Buscar todas las notas
+@app.get("/notes", tags=["Notes"])
+def search_notes(): 
+    return {"message": "Notas encontradas"}
 
+# Notes: Buscar nota por ID
+@app.get("/notes/{note_id}", tags=["Notes"])
+def search_notes_id(note_id: int):  # Usa get_db aquí
+    return {"message": "Nota encontrada"}
+
+# Notes: Actualizar nota
+@app.put("/notes/{note_id}", tags=["Notes"])
+def update_note(note_id: int):
+    return {"message": f"Nota {note_id} actualizada"}
+
+# Notes: Eliminar nota
 @app.delete("/notes/{note_id}", tags=["Notes"])
 def delete_note(note_id: int):
     return {"message": f"Nota {note_id} eliminada"}
-
--- models.py
+++ models.py
@@ -1,17 +1,22 @@
 from typing import Union
 from pydantic import BaseModel
 from datetime import date
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from fastapi import Depends
 
-class User(BaseModel):
-    name: str
-    gmail:str
-    password: float
-    is_active: Union[bool, None] = False
+class User(SQLModel, table=True):
+    name: str = Field(index=True)
+    gmail: str | None = Field(default=None)
+    password: str | None = Field(default=None, index=True)
+    idu: int | None = Field(default=None, primary_key=True)
 
-
-class Notes(BaseModel):
-    title: str
-    descripcion: str
-    date: date
-    user_id: int
     
+class Notes(SQLModel, table=True):
+    title: str = Field(index=True)
+    description: str | None = Field(default=None)
+    date: date
+    idn: int | None = Field(default=None, primary_key=True)   
+
+
+
+
